0a440c9a8c994769d0b019301049a0fb4125cd61
durianskh <durianskh@bji118-l8900676.geely.auto>
2020-03-09 09:54:12 +0800

    init

---
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,14 @@
+package $package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot application from maven-archetype by durianskh@gmail.com
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
